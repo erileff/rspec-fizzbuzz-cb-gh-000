dd03ea0c1246941f2cf184ced036bcb264458943
Erica Leff <ericaleff@gmail.com>
2018-04-16 03:38:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(fizz_int)
-  if fizz_int % 3 == 0
+def fizzbuzz(int)
+  if int % 3 == 0
     puts "Fizz"
-  elsif fizz_int % 5 == 0
+  elsif int % 5 == 0
   puts "Buzz"
-elsif fizz_int % 5 == 0 && fizz_int % 3 == 0
+elsif int % 5 == 0 && int % 3 == 0
   puts "FizzBuzz"
   end
 end
